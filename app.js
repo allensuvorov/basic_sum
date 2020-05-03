0babaea9ca74f21531fa744e7db2611d38d7bbb9
Allen <allenvs@mail.ru>
2020-05-03 17:59:24 +0300

    added solution code

---
+++ app.js
@@ -0,0 +1,13 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+// variables, input data
+let x = 22;
+let y = 33;
+
+// functions
+function basic_sum (a,b) {
+    return a+b;
+};
+
+// calling of functions
+console.log(basic_sum (x,y));
